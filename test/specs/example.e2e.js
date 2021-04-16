d9a9adc86d93275308372c1f586090f00fccba5a
avv <alexey_vasilenko@mail.ru>
2021-04-16 11:58:32 -0600

    third commit

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,45 @@
-import LoginPage from  '../pageobjects/login.page';
+import Page from  '../pageobjects/page';
 import ProfilePage from '../pageobjects/profile.page';
+import LoginPage from '../pageobjects/login.page';
 
 describe('Auth', () => {
-    it('user logs in with valid data', () => {
+    beforeEach(() => {
         LoginPage.open();
-        LoginPage.setLogin('kojidos915@whyflkj.com');
+        LoginPage.openLoginPageBtn();
+    });
+
+    afterEach( () => {
+        browser.execute('window.localStorage.clear()');
+    });
+
+    it('user logs in with valid data', () => {
+
+        // LoginPage.openLoginPageBtn();
+        LoginPage.setLogin('avbusi7@gmail.com');
         LoginPage.setPassword('Vamasha1');
         LoginPage.clickSubmitButton();
         ProfilePage.isOpen();
     });
+
+    it('submit button is disabled if login and password are absent', () => {
+
+        LoginPage.submitButtonIsDisabled();
+    });
+
+    it('fails of invalid data provided ', function () {
+        LoginPage.setLogin('example@example.com');
+        LoginPage.setPassword('123456');
+        LoginPage.clickSubmitButton();
+        LoginPage.errorToastAppeared();
+    });
+
+    it('login input is required', () => {
+        LoginPage.setLogin('example');
+        LoginPage.emptyLoginInput();
+        LoginPage.loginRequiredError();
+    });
+
+
 });
 
 
